09cf9ea9c35719068f7acb56f2091883640bb218
Amantchowdhary31 <amantch2@gmail.com>
2024-08-10 01:57:27 +0530

    created the admin panel and add data

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./component/Navbar/Navbar";
+import Sidebar from "./component/Sidebar/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add/Add";
+import List from "./pages/List/List";
+import Order from "./pages/order/order";
 
 const App = () => {
   return (
     <div>
-      admin
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add></Add>} />
+          <Route path="/list" element={<List></List>} />
+          <Route path="/orders" element={<Order></Order>} />
+        </Routes>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
