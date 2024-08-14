d94fac09201b27dee69a9188a070450f69cea792
Amantchowdhary31 <amantch2@gmail.com>
2024-08-14 17:42:09 +0530

    hffgfhn

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./verify.css";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
 
 const Verify = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const success = searchParams.get("success");
   const orderId = searchParams.get("orderId");
-  console.log(success, orderId);
+  const { url } = useContext(StoreContext);
+  const navigate = useNavigate();
+
+  const verifyPayment = async () => {
+    const response = await axios.post(url + "/api/order/verify", {
+      success,
+      orderId,
+    });
+    if (response.data.success) {
+      navigate("/myorder");
+    } else {
+      navigate("/");
+    }
+  };
 
   return (
     <div className="verify">
