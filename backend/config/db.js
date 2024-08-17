d43979ca8a8990ae76bfbe4fa856bc4d5e2f9e38
Amantchowdhary31 <amantch2@gmail.com>
2024-08-18 01:20:58 +0530

    fix env

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://amantch2:9832110076@cluster0.olqrq.mongodb.net/food-del', {
-
-    });
+    await mongoose.connect(process.env.MONGO_URI, {});
     console.log('DB connected');
   } catch (error) {
     console.error('DB connection error:', error);
